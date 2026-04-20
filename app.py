fe87024d5d057478120e23973d0ca5367aad6960
Boppudi Omsaichand <omsaichandboppudi@gmail.com>
2026-04-20 19:10:33 +0530

    changed the info of the code

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.rout('/')
 def hello():
     build_id = os.getenv('BUILD_ID', 'Local')
-    return f"<h1>Hello, world from flask!</h1><p>Deploy via jenkins webhook. Build ID: {build_id}</p>"
+    return f"<h1>Hello, world from flask!</h1><p>Deploy via jenkins webhook. Build  ID: {build_id}</p>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
